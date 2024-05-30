465903b6e3d4d9007cca65370171ad3ffb35873f
USER 1 <gchkumar999@gmail.com>
2024-05-30 12:19:34 +0530

    datatypes

---
+++ Datatypes/one.js
@@ -0,0 +1,17 @@
+// create 
+
+var eid=101;
+var ename="Rahul";
+var esal=45000;
+
+//read
+
+console.log(eid);
+console.log(ename);
+console.log(esal);
+
+// how to find the data
+
+console.log(typeof eid);
+console.log(typeof ename);
+console.log(typeof esal);
