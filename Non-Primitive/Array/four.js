6c7514a135e38dc7a4d2b3fa24fbe5a40bc270ca
USER 1 <gchkumar999@gmail.com>
2024-05-31 12:29:08 +0530

    learning nonprimitive-DT

---
+++ Non-Primitive/Array/four.js
@@ -0,0 +1,11 @@
+let enames=['Rahul','Kumar','Varma','Arun','Amith']
+//index        0       1        2      3      4 
+console.log(enames);
+
+// how to read array elements - using indexing
+
+console.log(enames[0]);
+console.log(enames[1]);
+console.log(enames[2]);
+console.log(enames[3]);
+console.log(enames[4]);
