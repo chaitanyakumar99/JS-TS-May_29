b4d66cb972ae0dc27c4a26a727e5d05d7a59c400
USER 1 <gchkumar999@gmail.com>
2024-05-31 12:33:05 +0530

    learning nonprimitive-DT

---
+++ Non-Primitive/Array/five.js
@@ -0,0 +1,17 @@
+let enames=['Rahul','Kumar','Varma','Arun','Amith']
+//index        0       1        2      3      4 
+
+// how to read array elements - using indexing
+
+console.log(enames[0]);
+console.log(enames[1]);
+console.log(enames[2]);
+console.log(enames[3]);
+console.log(enames[4]);
+console.log(enames[7]); //undifined
+
+
+//update operation
+
+ enames[0]="Rahul Gandhi"
+ console.log(enames);
