8f3f94f693c0405f9b9575c3ab74a39df554f204
USER 1 <gchkumar999@gmail.com>
2024-06-01 21:52:42 +0530

    learning operators

---
+++ Operators/data-1.js
@@ -0,0 +1,15 @@
+
+let a=10;
+let b=60.5;
+let c="40";
+let d="Rahul";
+let e=true;    // true 1
+let f=false;   // false 0
+let g;
+
+console.log(a+b);  // 70.5
+console.log(a+c);  // 1040
+console.log(a+d);  // 10Rahul
+console.log(a+e);  // 11
+console.log(a+f);  // 10 
+console.log(a+g);  // NaN
