50fe66901ce0a3d8dd4d3971ae8d193a0168a95f
USER 1 <gchkumar999@gmail.com>
2024-06-02 20:43:27 +0530

    learning operators

--- Operators/logical.js
+++ Operators/logical.js
@@ -3,6 +3,8 @@ let eage = 22;
 
 // console.log(esal>40000 && eage >26); // false
 
+// if the condition is satisfied if-statement will be excuted  (otherwise) not satisfied else-statement will be excuted
+
 if(esal>5000 && eage>20){
 console.log("Eligible");
 }
