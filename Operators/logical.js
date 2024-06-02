6e0373c0b56ceec41e64ddf69fbd2e213a0497da
USER 1 <gchkumar999@gmail.com>
2024-06-02 20:35:41 +0530

    learning operators

--- Operators/logical.js
+++ Operators/logical.js
@@ -1,4 +1,11 @@
 let esal = 45000;
-let eage = 21;
+let eage = 22;
 
-console.log(esal>40000 && eage >26); // false
+// console.log(esal>40000 && eage >26); // false
+
+if(esal>5000 && eage>20){
+console.log("Eligible");
+}
+else{
+    console.log("Not Eligible");
+}
