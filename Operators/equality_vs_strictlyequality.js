4fa465bd64a0558dcdfbe930151d274583749af2
USER 1 <gchkumar999@gmail.com>
2024-06-02 20:22:11 +0530

    learning operators

---
+++ Operators/equality_vs_strictlyequality.js
@@ -0,0 +1,19 @@
+
+// Equality operators == :- verify content
+
+// strict Equality operator === :- verify data type and content
+
+let a = 10;
+let b = '10';
+let c = 20
+
+console.log(a == b); //true
+console.log(a == c); //false
+
+
+// ---------------------strict equality examples----------------------
+
+console.log("strict equality");
+
+console.log(a === b); //false
+console.log(a === c); //false
