6cae3100e0bebb36c2ea3baf7cf996061635f052
USER 1 <gchkumar999@gmail.com>
2024-06-03 11:43:22 +0530

    prat

---
+++ Conditionalflow/three_digit_nbr.js
@@ -0,0 +1,11 @@
+
+// wap to check given number is three digit number or not 
+
+let num=450
+
+if(num>=100 && num<=999){  // && true if both the condition is  true
+   console.log("Given number is 3 digit number");
+}
+else{
+    console.log("Given number is not a 3 digit number");
+}
