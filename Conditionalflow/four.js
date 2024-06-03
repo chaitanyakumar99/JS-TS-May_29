f56c79cb117115b9ca3434132bb43be5cd17a441
USER 1 <gchkumar999@gmail.com>
2024-06-03 12:11:12 +0530

    prat

--- Conditionalflow/four.js
+++ Conditionalflow/four.js
@@ -1,8 +1,24 @@
 let ename="Rahul"
 
+// if the condition is true 1 :- if the condition is true if-block will be excuted 
+
+// if the condition is false 0 :- if the condition is false else-block will be excuted 
+
+
+
 if(1){
  console.log("GM");
 }
 else{
     console.log("GN");
 }
+
+// --------------------------------------------------------------------------------
+
+
+// if(0){
+//     console.log("GM");
+// }
+// else{
+//     console.log("GN");
+// }
