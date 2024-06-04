fa89773deced0370528fbde1b9f48f7d0a7c0479
USER 1 <gchkumar999@gmail.com>
2024-06-04 23:15:56 +0530

    prat

--- Conditionalflow/three_digit_nbr.js
+++ Conditionalflow/three_digit_nbr.js
@@ -3,6 +3,9 @@
 
 let num=450
 
+// if the condition is true if-block will be excuted 
+
+//  if the condition is false else-block will be excuted 
 
 if(num>=100 && num<=999){  // && true if both the condition is  true
    console.log("Given number is 3 digit number");
