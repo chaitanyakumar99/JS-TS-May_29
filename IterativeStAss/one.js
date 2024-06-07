5c8563e0d9fd1e263b3eda78ab35b173ffd27b02
USER 1 <gchkumar999@gmail.com>
2024-06-07 17:32:15 +0530

    learning basics

--- IterativeStAss/one.js
+++ IterativeStAss/one.js
@@ -1,6 +1,13 @@
 let employees=[{id:101,name:"Rahul",sal:45000},{id:102,name:"Kumar",sal:55000},{id:103,name:"Varma",sal:75000}
 ]
 
-for(employee of employees){
-console.log(`Employee Id: ${employee.id} and Name: ${employee.name}`);
+// for(employee of employees){
+// console.log(`Employee Id: ${employee.id} and Name: ${employee.name}`);
+// }
+
+
+let i=0;
+while(i<=employees.length -1 ){
+    console.log(`Employee Id: ${employees[i].id} and Name: ${employees[i].name}`);
+    i++
 }
