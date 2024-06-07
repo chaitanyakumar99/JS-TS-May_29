94a2b8926da7957ff89aad696b76f0dcad1848ab
USER 1 <gchkumar999@gmail.com>
2024-06-07 17:20:19 +0530

    learning basics

---
+++ IterativeStAss/one.js
@@ -0,0 +1,6 @@
+let employees=[{id:101,name:"Rahul",sal:45000},{id:102,name:"Kumar",sal:55000},{id:103,name:"Varma",sal:75000}
+]
+
+for(employee of employees){
+console.log(`Employee Id: ${employee.id} and Name: ${employee.name}`);
+}
