bf26b5b63a0ad2950d87bb1dce5a4275f6c4900e
USER 1 <gchkumar999@gmail.com>
2024-06-07 12:38:03 +0530

    prat

---
+++ iterative_St/for_condition.js
@@ -0,0 +1,11 @@
+
+let eids=[101,102,103,104,105,106]
+let enames=["Rahul","Kumar","Vijay","Arun"]
+
+for(eid of eids){
+    console.log(eid);
+}
+
+for (ename of enames){
+    console.log(ename);
+}
