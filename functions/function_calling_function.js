acfe2db3f4d08f4639d14b7c87951c5378feac1c
USER 1 <gchkumar999@gmail.com>
2024-06-08 19:30:30 +0530

    learning basics

---
+++ functions/function_calling_function.js
@@ -0,0 +1,11 @@
+function wish(){
+    console.log("Good Morning");
+    console.log("Good Afternoon");
+    console.log("Good Night");
+}
+
+// calling function with function name
+
+wish()
+wish()
+wish()
