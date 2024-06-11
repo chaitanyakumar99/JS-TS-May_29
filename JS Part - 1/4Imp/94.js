436f2cfd8e63c1ed42c2cfb776aed8131a83939c
USER 1 <gchkumar999@gmail.com>
2024-06-11 15:01:17 +0530

    tst'

---
+++ JS Part - 1/4Imp/94.js
@@ -0,0 +1,8 @@
+
+
+let eids=[101,102,103,104,105,106,107,108,109,110,111,112]
+//index    0    1   2   3   4                          n-1
+
+console.log(eids.pop());
+
+//console.log(eids[eids.length-1]);
