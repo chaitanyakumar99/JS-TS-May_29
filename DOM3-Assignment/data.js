d15793e6f226665ae6c23659169228fa3d16fe15
USER 1 <gchkumar999@gmail.com>
2024-06-13 16:15:54 +0530

    on mouseover change color

---
+++ DOM3-Assignment/data.js
@@ -0,0 +1,7 @@
+function changeColor(){
+    // alert('Text case 123')   //for testing purpose
+    let btn_Ref=document.getElementsByTagName('button')[0]
+    console.log(btn_Ref);
+    btn_Ref.style.backgroundColor='green'
+    
+}
