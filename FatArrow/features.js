b5161e92c5f430fbb505b5d51c70f87170b046c5
USER 1 <gchkumar999@gmail.com>
2024-06-14 23:35:34 +0530

    learning functions and fat arrow functions

--- FatArrow/features.js
+++ FatArrow/features.js
@@ -1,4 +1,15 @@
-function wish(){
-    console.log(this); // this :- this is the pointer to point out the current object
+
+// functions
+
+// function wish(){
+//     console.log(this); // this :- this is the pointer to point out the current object
+// }
+// wish()
+
+
+//fat arrow function 
+
+let add=()=>{
+    console.log(this);
 }
-wish()
+add()
