e1cc44690c505779c9e94902c9d28de47f3849a8
USER 1 <gchkumar999@gmail.com>
2024-06-15 18:53:00 +0530

    learning functions and fat arrow functions

--- FatArrow/this-ex.js
+++ FatArrow/this-ex.js
@@ -1,5 +1,11 @@
+//this :- this is the pointer pointing to the  current object
 
-function print(){
+// function print(){
+//     console.log(this);
+// }
+// print()
+
+let print=()=>{
     console.log(this);
 }
 print()
