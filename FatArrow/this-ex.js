c63d2d15cf1fe8b2ebbba945ef98f9f5a5cacbcb
USER 1 <gchkumar999@gmail.com>
2024-06-15 19:10:10 +0530

    prat

--- FatArrow/this-ex.js
+++ FatArrow/this-ex.js
@@ -1,9 +1,15 @@
 //this :- this is the pointer pointing to the  current object
 
+// function
+
+
 // function print(){
 //     console.log(this);
 // }
 // print()
+
+
+//fat arrow function 
 
 let print=()=>{
     console.log(this);
