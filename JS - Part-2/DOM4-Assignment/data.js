50d02b6387a2492cc769559029a2e00487d13a7c
USER 1 <gchkumar999@gmail.com>
2024-06-20 19:44:57 +0530

    prat

---
+++ JS - Part-2/DOM4-Assignment/data.js
@@ -0,0 +1,9 @@
+function changeColor(){
+
+    //alert("Test Case 123")
+    console.log("Test Case 123")
+    let input_Tag_Ref = document.getElementById('abc')
+    console.log(input_Tag_Ref)
+    input_Tag_Ref.style.backgroundColor = "yellow";
+    
+}
