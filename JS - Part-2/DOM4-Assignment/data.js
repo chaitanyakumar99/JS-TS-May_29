06f3a9bf256fc17b4fbd17cbfcc4486907ecd78d
USER 1 <gchkumar999@gmail.com>
2024-06-21 16:24:45 +0530

    prat

--- JS - Part-2/DOM4-Assignment/data.js
+++ JS - Part-2/DOM4-Assignment/data.js
@@ -1,9 +1,19 @@
 function changeColor(){
 
     //alert("Test Case 123")
+
     console.log("Test Case 123")
     let input_Tag_Ref = document.getElementById('abc')
     console.log(input_Tag_Ref)
     input_Tag_Ref.style.backgroundColor = "yellow";
     
+}
+
+
+let changeCase=()=>{
+//alert('Text Case 123')
+let input_Tag_Ref =document.getElementByTagName('input')[1]
+//console.log(input_Tag_Ref);
+let uname =input_Tag_Ref.value;
+input_Tag_Ref.value=uname.toUpperCase()
 }
