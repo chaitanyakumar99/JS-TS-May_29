c7058d5e097d74c7f90d097dff341aa60c576f4d
USER 1 <gchkumar999@gmail.com>
2024-06-24 16:40:27 +0530

    prat

---
+++ OOP-1/two.js
@@ -0,0 +1,15 @@
+class Account {
+    acc_id = 101;
+    acc_Name = "Rahul"
+    open_Account() {
+        console.log("Account oopened successfully");
+    }
+
+}
+
+let a1=new Account();
+console.log(a1);
+console.log(a1.acc_id);
+console.log(a1.acc_Name);
+
+a1.open_Account()
