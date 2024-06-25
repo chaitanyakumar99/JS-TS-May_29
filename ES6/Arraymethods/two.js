315b96b38ccd79b19c3ec4bd8146381570b97d76
USER 1 <gchkumar999@gmail.com>
2024-06-25 22:32:38 +0530

    prat

---
+++ ES6/Arraymethods/two.js
@@ -0,0 +1,10 @@
+let prices=[100,200,300,400,500]
+
+//create newprices variable by adding +1
+
+let new_prices=prices.forEach((price)=>{     //udefined
+    return price +1
+})
+
+console.log(prices);
+console.log(new_prices);
