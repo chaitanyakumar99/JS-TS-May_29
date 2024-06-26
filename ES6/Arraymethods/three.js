fe685d650b74c2462d49ae55b84224421531a9ac
USER 1 <gchkumar999@gmail.com>
2024-06-26 12:27:36 +0530

    prat

---
+++ ES6/Arraymethods/three.js
@@ -0,0 +1,12 @@
+let numbers=[1,2,3,4,5,6,7,8,9,10]
+
+let even_numbers=[]
+
+for(num of numbers){
+    if(num%2==0){
+        even_numbers.push(num)
+    }
+}
+
+console.log(numbers);
+console.log(even_numbers);
