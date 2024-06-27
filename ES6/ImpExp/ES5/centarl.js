f1094140c5d8dca10142f20ddb2386458f8c9993
USER 1 <gchkumar999@gmail.com>
2024-06-27 17:50:35 +0530

    prat

---
+++ ES6/ImpExp/ES5/centarl.js
@@ -0,0 +1,8 @@
+
+//ES5
+
+let tax=10;
+
+module.exports =tax       //ES5
+
+// export default tax;    //ES6
---
+++ ES6/ImpExp/ES5/ka.js
@@ -0,0 +1,6 @@
+
+let tax=require("./centarl")         //ES5
+
+// import tax from './centarl.js'    //ES6
+
+console.log(tax);
