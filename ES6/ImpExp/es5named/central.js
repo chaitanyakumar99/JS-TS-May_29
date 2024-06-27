9762b789306438e4a12b7f8bc009fe7bcb8c7e8d
USER 1 <gchkumar999@gmail.com>
2024-06-27 18:51:28 +0530

    txt

---
+++ ES6/ImpExp/es5named/central.js
@@ -0,0 +1,15 @@
+
+let tax =18
+
+let discount=5
+
+function add(){
+    console.log("Addition");
+}
+class Account{
+    constructor(){
+        console.log("Central File - Account Contructuro");
+    }
+}
+
+module.exports={Account,tax}
