11f1a22b076e4cdfe6682887f47557962bcce0bd
USER 1 <gchkumar999@gmail.com>
2024-06-28 12:32:53 +0530

    txt

---
+++ ES6/spread_ex/two.js
@@ -0,0 +1,17 @@
+let eids=[101,102,103,104]
+
+//index    0    1   2   3
+
+
+//create new Arraay based on exist array
+//group of elements as one entity
+//elements stored based on index
+
+
+let new_Eids=[]
+
+
+console.log(eids);
+console.log(new_Eids);
+
+
