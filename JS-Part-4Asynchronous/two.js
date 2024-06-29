d23fcac92219d5c8a069a58b1bc4d8c1a18050be
USER 1 <gchkumar999@gmail.com>
2024-06-29 10:33:52 +0530

    txt

---
+++ JS-Part-4Asynchronous/two.js
@@ -0,0 +1,20 @@
+setTimeout(() => {
+    console.log("Req 1");
+}, 1000);
+
+
+setTimeout(()=>{
+    console.log("Req 2");
+},2000)
+
+
+
+setTimeout(()=>{
+    console.log("Req 3");
+},3000)
+
+
+
+setTimeout(()=>{
+    console.log("Req 4");
+},4000)
